2975ca02fb286db0f663c0f0ab385ffd9ebfe88f
aslam7 <aslam.iiitb407@gmail.com>
2017-11-29 21:24:09 +0530

    first commit

---
+++ src/attendance/Attendance.java
@@ -0,0 +1,12 @@
+package attendance;
+
+public class Attendance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AttendanceView theView = new AttendanceView();
+		AttendanceModel theModel = new AttendanceModel();
+		AttendanceController theCon = new AttendanceController(theView, theModel);
+	}
+
+}
